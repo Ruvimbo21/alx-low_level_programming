3f0783c57b365f57e681057ae0daf44ab9c6f4f2
ruvimbo <ruvimbo9mawoyo@gmail.com>
2022-09-14 15:05:23 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print putchar
- * Return: always to 0
+ * Return: always 0
 */
 
 int main(void)
@@ -11,11 +11,11 @@ int main(void)
         _putchar('p');
         _putchar('u');
         _putchar('t');
-        _putchar('c')
+        _putchar('c');
         _putchar('h');
         _putchar('a');
         _putchar('r');
-        _putchar('/n');
+        _putchar('\n');
 
        return (0);
 
